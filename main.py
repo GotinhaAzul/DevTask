0cc3375e19ffcc8c0d7a816999e6d2cc66ac277d
Davi Santiago <d4v1santiago@gmail.com>
2026-07-08 10:10:18 -0300

    Added 'read' function

--- main.py
+++ main.py
@@ -2,37 +2,49 @@ import json
 
 
 def create(nome, id):
-    dados = {"nome": nome, "id": id}
+    dados = {"nome": nome, "id": id, "done": False}
 
-    # Abre o arquivo como "file"
-    with open("dados.json", "r", encoding="utf-8") as file:
+    with open("dados.json", "r") as file:  # Abre o arquivo como "file"
         data = json.load(file)
 
-    # Adiciona os novos dados
-    data["tasks"].append(dados)
+    data["tasks"].append(dados)  # Adiciona os novos dados
 
-    # Salva os dados atualizados
-    with open("dados.json", "w") as file:
+    with open("dados.json", "w") as file:  # Salva os dados atualizados
         json.dump(data, file, indent=4)
 
 
+def read(id=None):
+    with open("dados.json", "r") as file:
+        tarefas = json.load(file)
+
+        if id is not None:
+            for i in tarefas["tasks"]:
+                if i["id"] == id:
+                    print(i)
+
+        else:
+            for i in tarefas["tasks"]:
+                print(i)
+
+
 def main():
-    print("1. Criar Tarefa")
-    print("2. Listar Tarefa")
-    print("3. Editar Tarefa")
-    print("4. Excluir Tarefa")
+    print("1. Criar Tarefa")  # Crie tarefas
+    print("2. Listar Tarefa")  # Veja e conclua tarefas
+    print("3. Editar Tarefa")  # Edite tarefas
+    print("4. Excluir Tarefa")  # Exclua tarefas
     esc = str(input(""))  # Linha vazia para o usuário ter a experiência "Interativa".
 
     if esc == "1":
         nome = str(input("Nome da tarefa a criar: "))
         arquivo = open("dados.json", "r")
         id = 0
-        for i in arquivo:  # Isso aqui não é uma boa opção. IDs
+        for i in arquivo:  # Isso aqui não é uma boa opção. IDs ficam estranhos.
             id += 1
         create(nome, id)
 
     elif esc == "2":
-        pass
+        read()
+
     elif esc == "3":
         pass
     elif esc == "4":
